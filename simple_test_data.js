979e7f25d1aa6f34da1069b8322f8a8a700060c9
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2024-08-09 11:51:30 -0500

    Create simple_test_data.js

---
+++ simple_test_data.js
@@ -0,0 +1,18 @@
+[
+    {
+        "text": "This is a positive example.",
+        "label": "positive"
+    },
+    {
+        "text": "This is a negative example.",
+        "label": "negative"
+    },
+    {
+        "text": "This is another positive example.",
+        "label": "positive"
+    },
+    {
+        "text": "This is another negative example.",
+        "label": "negative"
+    }
+]
